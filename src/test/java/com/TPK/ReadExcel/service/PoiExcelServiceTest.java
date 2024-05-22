1808a7ba31cbc4af13cc135a9bfc026afca28ee0
taknaja <peerapolkoeram@gmail.com>
2024-05-22 19:31:07 +0700

    Test Stream

--- src/test/java/com/TPK/ReadExcel/service/PoiExcelServiceTest.java
+++ src/test/java/com/TPK/ReadExcel/service/PoiExcelServiceTest.java
@@ -24,6 +24,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -118,6 +119,40 @@ class PoiExcelServiceTest {
         assertNotEquals("2", resultPoi.get(0).column1());
         assertNotEquals("2", resultPoi.get(0).column2());
         assertNotEquals("2", resultPoi.get(0).column3());
+    }
+
+    @Test
+    void TestFunction() {
+        Function<String, Integer> strToInt = Integer::parseInt;
+        int result = strToInt.apply("222");
+        System.out.println(result);
+    }
+
+    @Test
+    void TestFunctionComposeAndThen() {
+        Function<Integer, Integer> nultiple2 = i ->  i * 2;
+        Function<Integer, Integer> plus2 = i ->  i + 2;
+        int result1 = plus2.compose(nultiple2).apply(8);
+        result1 = plus2.andThen(nultiple2).apply(8);
+        System.out.println(result1);
+    }
+
+    @Test
+    void TestStreamForEach() {
+        List<String> arr = List.of("Test","Test2","Test3","Mount","And");
+        // loop all
+        arr.stream().forEach(System.out::println);
+        System.out.println("--------");
+        // filter
+        List<String> arr2 = arr.stream().filter( a -> a.contains("Test")).toList();
+        arr2.stream().forEach(System.out::println);
+        System.out.println("--------");
+        // sorted
+        List<String> arr3 = arr.stream().sorted().toList();
+        arr3.stream().forEach(System.out::println);
+        System.out.println("--------");
+        // anyMatch
+        System.out.println(arr.stream().anyMatch(a -> a.startsWith("G")));
     }
 
 }
